a31a88f543501d2eeae5313034cf757fc7fa432f
kaisar12012000 <71655666+kaisar12012000@users.noreply.github.com>
2021-06-09 10:11:04 +0530

    Add files via upload

---
+++ assets/projects/project/3and4.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../../css/index.css">
+    <title>Flask Blog</title>
+</head>
+<body>
+    <nav class="navbar">
+        <ul>
+            <li><a href="../../../index.php" style="text-decoration:none; color:black;">Portfolio</a></li>
+            <!--<li><a href="../../../index.php/#Contacts" id="scrollClick" class="nav-links hireMe">Hire Me</a></li>-->
+        </ul>
+    </nav>
+    <div id="3" class="container jumbotron">
+        <img src="../../images/HTML_CSS_BS.png" alt="phpProject">
+        <span class="text-center">An ecommerse website</span>
+        <p>This project is an ecommerse website. The basic functionalities are that you can order and your orders are stored in the database.
+        The fronted comprises os HTML 5, CSS, Bootstrap. The backend comprises of PHP with SQL for the database management.
+        </p>
+    </div>
+    <div id="4" class="container jumbotron">
+        <img src="../../images/flaskblog.png" alt="flaskProject">
+        <span class="text-center">A blogging website</span>
+        <p>This project is a bloging website. Here the user can register to create and account, login with the appropriate credentials and then post
+        his or her textual content. Other account holders can see his or her posts but cannot edit them. The posts can be edited only by the user
+        who posted it. The frontend comprises of HTML 5, CSS, Bootstrap. The backed ius is completely based on flask.
+        </p>
+    </div>
+</body>
+</html>
