e27223e85ecfb714b5b82f50ef8938294072b584
kaisar12012000 <71655666+kaisar12012000@users.noreply.github.com>
2021-06-09 09:58:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="assets/css/index.css">
+        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+        <title>Portfolio</title>
+    </head>
+    <!-------------------------------Header-------------------------------->
+    <header>
+        <nav class="navbar">
+            <ul>
+                <li>Portfolio</li>
+                <li><a href="#Contacts" id="scrollClick" class="nav-links hireMe">Hire Me</a></li>
+            </ul>
+        </nav>
+        <div class="banner row col-lg-12">
+            <div class="banner1 display-5 col-lg-6 col-xs-12">
+                <span><b>
+                    Hi,<br>
+                    Myname is Irshaduddin Chowdhury.<br>
+                    Welcome to my portfolio website..</b></span><br>
+                <div class="row col-lg-12">
+                    <a href="assets/Resume.pdf" class="downCV col-lg-3 col-xs-12" target="_blank" download="irshaduddin_chowdhury_resume">Resume</a>
+                    <a href="#Contacts" id="scrollClick" class="hireMe col-lg-3 col-xs-12">Hire Me</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!----------------------------About Me----------------------------------->
+    <section class="about row jumbotron col-lg-12 col-xs-12">
+        <div class="col-lg-4 col-xs-12">
+            <img class="rounded mx-auto d-block" src="assets/images/Irshad-Profile.jpg" style="width: 250px; height: 300px; border-radius: 50%" alt="ProfilePic">
+        </div>
+        <div class="col-lg-8 cl-xs-12">
+            <span style="font-size: 42px"><b>About me: </b></span><br><span style="font-size: 36px">My name is <u>Irshaduddin Chowdhury</u></span>
+            <p> I am currently pursuing B.Tech in mechanical Engineering. I have done a training on web devolopment from <a href="https://www.google.com/search?q=internshala&oq=inter&aqs=chrome.1.69i60j69i59j69i57j69i60l2j69i65j69i60l2.8883j1j7&sourceid=chrome&ie=UTF-8" target="_blank">Internshala.</a>
+                     My training included modules of <a href="https://g.co/kgs/yQg2mr" target="_blank">HTML5</a>, <a href="https://g.co/kgs/BZC6zd" target="_blank">CSS</a>, <a href="https://g.co/kgs/tEMyjP" target="_blank">Bootstrap</a>, <a href="https://g.co/kgs/Wt9gME" target="_blank">PHP</a> and <a href="https://g.co/kgs/nrjZXK" target="_blank">SQL</a>.
+                      In the end of each module a test was conducted and at the end of the entire training program a final test was conducted that tested my application skills of the modules mentioned.</p>
+                <a href="#projects" id="scrollClick" style="font-weight: 400" class="downCV">Projects</a> <a id="scrollClick" href="#Contacts" style="font-weight: 400" class="hireMe">Contact Me</a></p>
+        </div>
+    </section>
+    <!----------------------------skills----------------------------------->
+    <section class="container skills">
+        <span class="text-center col-lg-12 col-xs-12" style="font-size: 42px; margin:20px auto;"><b>Skills...</b></span>
+        <div class="row col-lg-12 col-xs-12 row1">
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>HTML</b></span><br>
+                <small>Level: Advanced</small>
+                <p>Creating the skeleton of the webpage. Basic building block for the webpage.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>CSS</b></span><br>
+                <small>Level: Advanced</small>
+                <p>Designing and maiking the page resposive. Adding style to the page.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>Bootstrap</b></span><br>
+                <small>Level: Advanced</small>
+                <p>Designing and maiking the page resposive. Adding style to the page.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+        </div>
+        <div class="row col-lg-12 col-xs-12 row1">
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>PHP</b></span><br>
+                <small>Level: Intermediate</small>
+                <p>Server side scripting.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>SQL, MySQL</b></span><br>
+                <small>Level: Intermediate</small>
+                <p>Managing data input and output stored in the database.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>JAVASCRIPT</b></span><br>
+                <small>Level: Intermediate</small>
+                <p>Front-end scripting. Responsive and interactive page.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+        </div>
+        <div class="row col-lg-12 col-xs-12 row1">
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>PYTHON</b></span><br>
+                <small>Level: Advanced</small>
+                <p>Backend functionalities of the website.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>FLASK</b></span><br>
+                <small>Level: Intermediate</small>
+                <p>Micro web framework written in python.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+            <div class="skillCard col-lg-3 col-xs-12 text-center">
+                <span><b>REACT & REDUX</b></span><br>
+                <small>Level: Intermediate</small>
+                <p>Front-end scripting. Single page applications.</p>
+            </div>
+            <div class="col-lg-1 col-xs-12"></div>
+        </div>
+    </section>
+    <!----------------------------Projects----------------------------------->
+    <section id="projects" class="projects container">
+        <center class="col-lg-12 col-xs-12" style="font-size: 42px; margin:20px auto;"><b>Projects</b></center>
+        <div class="container">
+          <h2 class="text-center">Have a look at my projects and let me know what you think.</h2>
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+
+              <div class="item active">
+                <img src="assets/images/JS_Project1_pic.png" alt="Los Angeles" style="width:100%;">
+                <div class="carousel-caption">
+                  <h3><a href="assets/projects/Project1/JS_Project1.html" target="_blank">World Clock</a></h3>
+                  <p>Get to know the time at your city</p>
+                </div>
+              </div>
+
+              <div class="item">
+                <img src="assets/images/JS_project2.png" alt="Chicago" style="width:100%;">
+                <div class="carousel-caption">
+                  <h3><a href="assets/projects/ToDo_List.html" target="_blank">To Do list</a></h3>
+                  <p>Never forget your tasks</p>
+                </div>
+              </div>
+
+              <div class="item">
+                <img src="assets/images/HTML_CSS_BS.png" alt="New York" style="width:100%;">
+                <div class="carousel-caption">
+                  <h3><a href="assets/projects/Project4/3and4.php">A marketing site</a></h3>
+                  <p>Shop here to experience best services</p>
+                </div>
+              </div>
+              <div class="item">
+                <img src="assets/images/flaskblog.png" alt="New York" style="width:100%;">
+                <div class="carousel-caption">
+                  <h3><a href="assets/projects/Project4/3and4.php">A Blogging website</a></h3>
+                  <p>Express your thoughts here</p>
+                </div>
+              </div>
+              <div class="item">
+                <img src="assets/images/cowinportal.png" alt="New York" style="width:100%;">
+                <div class="carousel-caption">
+                  <h3><a href="https://cowin.codekaroyaaro.com/">Vaccination registration portal</a></h3>
+                  <p>Worked as mentor for 11 years kids in this project.</p>
+                </div>
+              </div>
+
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+    </section>
+    <!-------------------------------------------------Comments-------------------------------------------------------------------->
+    <div class="comments">
+        <center>
+            <p class="comments1"><b>Liked my work so far???</b></p>
+            <p class="comments2">Please express your views and help me grow..</p>
+            <button class="comments-btn" onclick="location.href = 'comments.php'; return false"><b>Leave your suggestions..</b></button>
+        </center>
+    </div>
+    <!-----------------------------------------------------Contact me------------------------------------------------------------------------------------>
+    <div class="contact" id="Contacts">
+        <span class="contact-h"><center><b>Contact me...</b></center></span>
+        <div class="contact-details row col-lg-12 col-xs-12">
+            <div class="email col-lg-6 col-xs-12">
+                <center>Email me on: kaisarnagpur@gmail.com</center><br>
+                <center><i class="glyphicon glyphicon-envelope" id="email_icon"></i></center>
+            </div>
+            <div class="call col-lg-6 col-xs-12">
+                <center>Call me on: +91 8080439867.</center><br>
+                <center><i class="glyphicon glyphicon-earphone" id="call_icon"></i></center>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------Footer--------------------------------------------------------------------------->
+    <footer class="col-lg-12 col-xs-12">
+        <center>&copy; Created by Irshaduddin Chowdhury. Contact me on +91 8080439867. All Images are free to use and share. No copyright issues.</center>
+    </footer>
+    <script src="assets/scripts/index.js"></script>
+</body>
+</html>
